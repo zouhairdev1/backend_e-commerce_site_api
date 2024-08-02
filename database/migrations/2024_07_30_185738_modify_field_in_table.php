42c59b683d9749af1a6c1655d274f3e92cd9845e
zouhair machmacha <zouhairmachmacha@gmail.com>
2024-08-02 13:39:18 +0200

    modify:table infos

---
+++ database/migrations/2024_07_30_185738_modify_field_in_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('infos', function (Blueprint $table) {
+            $table->string('city')->nullable()->change();
+            $table->string('adresse')->nullable()->change();
+            $table->string('country')->nullable()->change();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('infos', function (Blueprint $table) {
+            //
+        });
+    }
+};
