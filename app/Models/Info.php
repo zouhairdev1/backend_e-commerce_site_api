8fdb989e77cd7543e31b84e92d79fd976777f0aa
zouhair machmacha <zouhairmachmacha@gmail.com>
2024-08-02 13:50:13 +0200

    fix:config sluggable

--- app/Models/Info.php
+++ app/Models/Info.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Info extends Model
 {
-    use HasFactory;
+    use HasFactory,Sluggable;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'slug_user',
+        'tele',
+        'cin',
+        'city',
+        'country',
+        'adresse',
+        'role_id',
+        'user_id',
+        'email',
+        'password',
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -15,5 +30,13 @@ class Info extends Model
     public function roles()
     {
         return $this->belongsToMany(Role::class, 'role_user');
+    }
+    public function sluggable(): array
+    {
+        return [
+            'slug_user' => [
+                'source' => 'first_name'
+            ]
+        ];
     }
 }
